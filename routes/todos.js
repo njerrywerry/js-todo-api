32a06e55e2b92faed168937967221415793adfe9
Sylvia <sylviakieha@gmail.com>
2016-02-17 01:11:32 +0300

    GET /todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Todo = require('../models/Todo.js');
+
 /* GET todos listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Todo.find(function (err, todos) {
+    if (err) return next(err);
+    res.json(todos);
+  });
 });
 
 module.exports = router;
